5cb431238cf9bbfebd20b43193b13b84190b7545
rahul619302 <rahul.ab.singh@capgemini.com>
2018-11-18 09:16:14 +0530

    first commit

---
+++ src/test/java/com/cg/starter/HelloWorldApplicationTests.java
@@ -0,0 +1,48 @@
+package com.cg.starter;
+
+import com.cg.starter.controller.Controller;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class HelloWorldApplicationTests {
+
+    @Autowired
+    private Controller controller;
+    private MockMvc mockMvc;
+
+    /**
+     * Before run any test setup
+     */
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    /**
+     * Integration testing of helloworld method of controller
+     * @throws Exception
+     */
+    @Test
+    public void helloWorld() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders
+                .get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        Assert.assertThat("Hello World!", Is.is(response));
+    }
+
+}
